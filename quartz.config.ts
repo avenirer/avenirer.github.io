01885c793838a3983af57463058b42e93b45e921
Adrian Voicu <avenir.ro@gmail.com>
2025-04-09 09:33:37 +0300

    Quartz sync: Apr 9, 2025, 9:33 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Avenirer's Notes",
+    pageTitle: "Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
